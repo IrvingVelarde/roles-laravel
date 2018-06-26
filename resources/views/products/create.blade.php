ccdda3b363d0369c869bd03dbc95a88442edd234
Irvin Velarde <irvinvelarde@gmail.com>
2018-06-26 14:21:51 -0500

    CRUD Productos

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -5,12 +5,12 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Usuario</div>
+                <div class="panel-heading">Producto</div>
 
                 <div class="panel-body">                    
-                    {{ Form::open(['route' => 'users.store']) }}
+                    {{ Form::open(['route' => 'products.store']) }}
 
-                        @include('users.partials.form')
+                        @include('products.partials.form')
                         
                     {{ Form::close() }}
                 </div>
